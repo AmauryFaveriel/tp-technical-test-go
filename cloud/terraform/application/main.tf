a15e784076e645a17cb70ada9540e20fb2c42cae
amauryfaveriel <mrfvrl@gmail.com>
2020-04-22 12:44:48 +0200

    Add docker-compose template + finish ansible scripts

--- cloud/terraform/application/main.tf
+++ cloud/terraform/application/main.tf
@@ -76,7 +76,7 @@ resource "aws_elb" "application_elb" {
   availability_zones = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
 
   listener {
-    instance_port     = 8000
+    instance_port     = 8080
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
